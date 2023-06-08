4e59b9bab42827c16c3ac82f26879d0e57e0ea39
Wildan Mujahidin <wildan.mujahidin1@gmail.com>
2023-06-08 22:28:16 +0700

    add feature login and logout

--- src/components/Molekul/Header/index.jsx
+++ src/components/Molekul/Header/index.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import logo from "../../../assets/logo.png";
 import { Button, SelectOption } from "../../Atom";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const user = localStorage.getItem("email");
+
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("guest_session_id");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("guest_session_id");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
   return (
     <div className="h-20 flex justify-between items-center px-5">
       <div className="w-28">
@@ -13,9 +29,18 @@ const Header = () => {
 
       <div className=" flex gap-2">
         <SelectOption />
-        <Link to={"/login"}>
-          <Button placeholder={"Sign In"} />
-        </Link>
+        {isLoggedIn ? (
+          <div className="flex gap-2 align-middle items-center">
+            <p>{user.slice(0, 7)}</p>
+            <Link to={"/login"}>
+              <Button placeholder={"Log Out"} onClick={handleLogout} />
+            </Link>
+          </div>
+        ) : (
+          <Link to={"/login"}>
+            <Button placeholder={"Sign In"} />
+          </Link>
+        )}
       </div>
     </div>
   );
