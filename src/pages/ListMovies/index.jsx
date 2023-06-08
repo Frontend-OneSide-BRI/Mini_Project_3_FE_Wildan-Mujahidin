957de5ec765ea4c63caa775e95e2126a0c54a2a6
Wildan Mujahidin <wildan.mujahidin1@gmail.com>
2023-06-08 15:26:30 +0700

    add feature group by category in ListMovie

--- src/pages/ListMovies/index.jsx
+++ src/pages/ListMovies/index.jsx
@@ -25,6 +25,10 @@ const ListMovies = () => {
     dispatch(fetchFilterMovies(searchQuery));
   };
 
+  const handleCategoryMovie = (data) => {
+    dispatch(fetchFilterMovies(data));
+  };
+
   return (
     <div className="w-full h-screen flex flex-col overflow-auto">
       <div className="h-full w-full  overflow-auto">
@@ -44,26 +48,28 @@ const ListMovies = () => {
           </a>
         </div>
 
-        <div className="my-6 flex w-full px-5 gap-x-4 justify-center content-center">
-          <input
-            onChange={(event) => setSearchQuery(event.target.value)}
-            className="min-w-0 flex-auto rounded-md border-0 bg-black/50 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-            placeholder="Search Movie"
-          />
-          <Button placeholder={"Search"} onClick={handleSearchMovie} />
+        <div className="  flex justify-center content-center">
+          <div className="my-6 flex w-full lg:w-1/2 px-5 gap-x-4 justify-center content-center">
+            <input
+              onChange={(event) => setSearchQuery(event.target.value)}
+              className="min-w-0 flex-auto rounded-md border-0 bg-black/50 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              placeholder="Search Movie"
+            />
+            <Button placeholder={"Search"} onClick={handleSearchMovie} />
+          </div>
         </div>
 
-        <div className="flex justify-between gap-5 py-3 px-5">
-          <a>
+        <div className="flex justify-around gap-5 py-3">
+          <a onClick={() => handleCategoryMovie("action")}>
             <p>Action</p>
           </a>
-          <a>
+          <a onClick={() => handleCategoryMovie("adventure")}>
             <p>Adventure</p>
           </a>
-          <a>
+          <a onClick={() => handleCategoryMovie("animation")}>
             <p>Animation</p>
           </a>
-          <a>
+          <a onClick={() => handleCategoryMovie("comedy")}>
             <p>Comedy</p>
           </a>
         </div>
